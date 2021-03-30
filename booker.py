7afca5a533c2cbceff2097748f99613834a7930d
acinonyxjb <acinonyxjb@gmail.com>
2021-03-30 11:04:20 -0400

    doc

--- booker.py
+++ booker.py
@@ -1,29 +1,5 @@
 #!/usr/bin/env python3
-"""
-Automated reservation grabber for YMCA pools.
-
-Usage:
-  See the upcoming sessions
-  $ booker.py me@gmail.com mypassword mcburney-ymca upcoming
-
-  Book the next 2 upcoming slots at McBurney
-  $ booker.py me@gmail.com mypassword mcburney-ymca book 2
-
-The login / password are from the "YMCA of Greater New York" app.
-
-Requires python3, pandas, bs4 (beautifulsoup), requests
-
-Installation instructions:
-$ sudo apt-get install python3-pip
-$ pip3 install bs4 pandas requests
-
-To package a standalone binary:
-$ pip3 install pyinstaller
-$ pyinstaller booker.py --onefile --distpath .
-
-Which will create a 'booker' or 'booker.exe' binary that can be executed
-directly.
-"""
+"""Automated reservation grabber for YMCA pools."""
 
 import argparse
 import bs4
